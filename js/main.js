727167ca8ebc9dddf8a32e4e5cb68465d6737b53
PavloDaniielian <pdaniielian@gmail.com>
2025-02-21 16:40:54 +0200

    0.4

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-document.querySelectorAll('.section0-col-img1-container').forEach(container => {
-  const img = container.querySelector('img'); // Find the <img> inside the container
-  const originalSrc = img.src; // Store the original image src
+// document.querySelectorAll('.section0-col-img1-container').forEach(container => {
+//   const img = container.querySelector('img'); // Find the <img> inside the container
+//   const originalSrc = img.src; // Store the original image src
 
-  container.addEventListener('mouseenter', () => {
-    img.src = 'assets/images/page1/section0_tab_highlight.svg'; // Change to hover image
-  });
+//   container.addEventListener('mouseenter', () => {
+//     img.src = 'assets/images/page1/section0_tab_highlight.svg'; // Change to hover image
+//   });
 
-  container.addEventListener('mouseleave', () => {
-    img.src = originalSrc; // Revert back to original image on mouse leave
-  });
-});
+//   container.addEventListener('mouseleave', () => {
+//     img.src = originalSrc; // Revert back to original image on mouse leave
+//   });
+// });
